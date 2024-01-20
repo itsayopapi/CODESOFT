e248f457498391384cfc80b1ce4fbaf0c454c2c9
itsayopapi <gbemisolaalihassan@gmail.com>
2024-01-20 13:38:51 +0200

    Task 2

--- Task 2 - Landing Page/index.js
+++ Task 2 - Landing Page/index.js
@@ -1,8 +1,12 @@
-function openNavbar() {
-	document.getElementById("sideNavigationBar")
-		.style.width = "50%";
-}
-function closeNavbar() {
-	document.getElementById("sideNavigationBar")
-		.style.width = "0%";
-}
+const toggler = document.querySelector(".toggler");
+const navMenu = document.querySelector("#navMenu");
+
+toggler.addEventListener('click', function () {
+    navMenu.classList.toggle("active")
+});
+
+const scroll = document.getElementById("scroll");
+
+scroll.addEventListener('click', () => {
+    document.querySelector(".get-started").scrollIntoView({ behavior: 'smooth' });
+})
